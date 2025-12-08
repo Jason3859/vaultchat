f0e8f818902ef016b6aeff40ae1d5f00d3c82836
Jason3859 <augustinej438@gmail.com>
2025-12-08 13:50:11 +0530

    fixed vaultchat-admin-sdk.json file issue

--- vaultchat-server/src/main/kotlin/dev/jason/project/ktor/messenger/Application.kt
+++ vaultchat-server/src/main/kotlin/dev/jason/project/ktor/messenger/Application.kt
@@ -14,6 +14,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.netty.EngineMain
 import org.koin.ktor.plugin.Koin
+import java.io.File
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
@@ -43,12 +44,32 @@ private fun Application.initKoin() {
 }
 
 private fun Application.initFirebase() {
-    val serviceAccount = javaClass.classLoader
-        .getResourceAsStream("vaultchat-admin-sdk.json")
+    val file = adminSdkJsonFromEnvToFile()
+
+    val serviceAccount = file?.inputStream()
+        ?: javaClass.classLoader
+            .getResourceAsStream("vaultchat-admin-sdk.json")
 
     val options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
         .build()
 
     FirebaseApp.initializeApp(options)
+}
+
+private fun adminSdkJsonFromEnvToFile(): File? {
+    val content = System.getenv("ADMIN_SDK_JSON") ?: return null
+
+    val parent = File("ext-res")
+    val child = File(parent, "vaultchat-admin-sdk.json")
+
+    if (!parent.exists())
+        parent.mkdir()
+
+    if (!child.exists())
+        child.createNewFile()
+
+    child.writeText(content)
+
+    return child
 }
