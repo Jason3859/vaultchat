fb11684f7a6dabb278d5a115f52e9492ffdb8651
Jason3859 <augustinej548@gmail.com>
2026-03-07 11:00:19 +0530

    added spring server

---
+++ src/main/java/dev/jason/project/spring/vc_server/VcServerApplication.java
@@ -0,0 +1,13 @@
+package dev.jason.project.spring.vc_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jason/project/spring/vc_server/VcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jason.project.spring.vc_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
