f3bfe1b88d2cdb511c116a29f162de181f100b86
Jason3859 <augustinej548@gmail.com>
2026-03-12 15:59:31 +0530

    made uid id in users db

--- src/main/java/dev/jason/project/spring/vc_server/users/DBUser.java
+++ src/main/java/dev/jason/project/spring/vc_server/users/DBUser.java
@@ -1,10 +1,11 @@
 package dev.jason.project.spring.vc_server.users;
 
 import dev.jason.project.spring.vc_server.domain.User;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
-public record DBUser(String uid, String displayName, String profilePictureUrl, String fcmToken) {
+public record DBUser(@Id String uid, String displayName, String profilePictureUrl, String fcmToken) {
 
     public User toDomainUser() {
         return new User(uid, displayName, profilePictureUrl);
