03d3f4720495b46ec840972f513d12b045b1dc86
Jason3859 <augustinej548@gmail.com>
2026-03-16 14:48:30 +0530

    sends notifications without `com.google.firebase.messaging.Notification` class

--- src/main/java/dev/jason/project/spring/vc_server/domain/Message.java
+++ src/main/java/dev/jason/project/spring/vc_server/domain/Message.java
@@ -1,23 +1,16 @@
 package dev.jason.project.spring.vc_server.domain;
 
-import com.google.firebase.messaging.Notification;
 import dev.jason.project.spring.vc_server.service.VCUtilityService;
 
-import java.time.LocalDateTime;
-
-public record Message(String from, String to, String text) {
+public record Message(String from, String to, String text, String timestamp) {
 
     public com.google.firebase.messaging.Message toMessage(String toFcmToken) {
-        Notification firebaseNotification = Notification.builder()
-            .setTitle(String.format("New message from %s", VCUtilityService.getUserFromUid(from).displayName()))
-            .setBody(text)
-            .build();
-
         return com.google.firebase.messaging.Message.builder()
-            .setNotification(firebaseNotification)
             .setToken(toFcmToken)
+            .putData("title", String.format("New message from %s", VCUtilityService.getUserFromUid(from).displayName()))
+            .putData("body", text)
             .putData("uid", from)
-            .putData("timestamp", LocalDateTime.now().toString())
+            .putData("timestamp", timestamp)
             .build();
     }
 }
