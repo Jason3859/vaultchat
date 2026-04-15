b8ece3846b2f91d4560ccf0952bac331ac11a333
Jason3859 <augustinej548@gmail.com>
2026-04-15 14:25:12 +0530

    removed tests

--- src/test/java/dev/jason/project/spring/vc_server/TestConstants.java
+++
@@ -1,6 +0,0 @@
-package dev.jason.project.spring.vc_server;
-
-public class TestConstants {
-    public static final String TEST_USER_1 = "test-user-1";
-    public static final String TEST_USER_2 = "test-user-2";
-}
