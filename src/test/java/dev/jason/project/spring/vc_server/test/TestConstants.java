085343428aab32e2056c3ec111fe696b5e8a66a5
Jason3859 <augustinej548@gmail.com>
2026-04-29 10:06:05 +0530

    test controllers instead of service

--- src/test/java/dev/jason/project/spring/vc_server/test/TestConstants.java
+++ src/test/java/dev/jason/project/spring/vc_server/test/TestConstants.java
@@ -8,7 +8,6 @@ import dev.jason.project.spring.vc_server.model.Device;
 import dev.jason.project.spring.vc_server.model.Device.OS;
 import dev.jason.project.spring.vc_server.model.Device.Type;
 import dev.jason.project.spring.vc_server.model.Message;
-import dev.jason.project.spring.vc_server.model.User;
 
 public class TestConstants {
 	public static final Device TEST_DEVICE_1 =
@@ -20,41 +19,41 @@ public class TestConstants {
 	public static final Message DEFAULT_MESSAGE =
 		new Message("me", "to", "text", "timestamp");
 
-	public static final User TEST_USER_1 = getTestUser1(TEST_DEVICE_1);
+	public static final UserDto TEST_USER_1 = getTestUser1(TEST_DEVICE_1);
 
-	public static final User TEST_USER_2 = getTestUser2(TEST_DEVICE_2);
+	public static final UserDto TEST_USER_2 = getTestUser2(TEST_DEVICE_2);
 
-	public static final User UNKNOWN_USER_1 =
+	public static final UserDto UNKNOWN_USER_1 =
 		new UserDto(
 			"unknown_user_1",
 			"UNKNOWN_USER_1",
 			"url",
 			DeviceDto.fromDomain(TEST_DEVICE_1)
-		).asNewUser();
+		);
 
-	public static final User UNKNOWN_USER_2 =
+	public static final UserDto UNKNOWN_USER_2 =
 		new UserDto(
 			"unknown_user_2",
 			"UNKNOWN_USER_2",
 			"url",
 			DeviceDto.fromDomain(TEST_DEVICE_1)
-		).asNewUser();
+		);
 
-	public static User getTestUser1(Device device) {
+	public static UserDto getTestUser1(Device device) {
 		return new UserDto(
 			"test_user_1",
 			"Test User 1",
 			"url",
 			DeviceDto.fromDomain(device)
-		).asNewUser();
+		);
 	}
 
-	public static User getTestUser2(Device device) {
+	public static UserDto getTestUser2(Device device) {
 		return new UserDto(
 			"test_user_2",
 			"Test User 2",
 			"url",
 			DeviceDto.fromDomain(device)
-		).asNewUser();
+		);
 	}
 }
---
+++ src/test/java/dev/jason/project/spring/vc_server/test/UserSocialConnectionsControllerTests.java
@@ -0,0 +1,100 @@
+package dev.jason.project.spring.vc_server.test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import dev.jason.project.spring.vc_server.dto.UserDto;
+import dev.jason.project.spring.vc_server.repo.db.user.UserEntity;
+import dev.jason.project.spring.vc_server.repo.db.user.UserRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserSocialConnectionsControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository repository;
+
+    @BeforeEach
+    void setup() {
+        repository.saveAll(
+            Stream.of(TestConstants.TEST_USER_1, TestConstants.TEST_USER_2)
+                .map(UserDto::asNewUser)
+                .map(UserEntity::fromDomainUser).toList()
+        );
+    }
+
+    @AfterEach
+    void delete_test_users() {
+        repository.deleteAll(
+            Stream.of(TestConstants.TEST_USER_1, TestConstants.TEST_USER_2)
+                .map(UserDto::asNewUser)
+                .map(UserEntity::fromDomainUser).toList()
+        );
+    }
+
+    @Test
+    void connecting_two_existing_users_returns_accepted() throws Exception {
+        mockMvc.perform(patch("/user/social/connect")
+                .param("fromUid", TestConstants.TEST_USER_1.uid())
+                .param("otherUid", TestConstants.TEST_USER_2.uid()))
+            .andExpect(status().isAccepted());
+    }
+
+    @Test
+    void connecting_with_unknown_user_returns_not_found() throws Exception {
+        mockMvc.perform(patch("/user/social/connect")
+                .param("fromUid", TestConstants.TEST_USER_1.uid())
+                .param("otherUid", "unknown"))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void connecting_users_where_one_blocked_other_returns_forbidden() throws Exception {
+        mockMvc.perform(patch("/user/social/block")
+                .param("fromUid", TestConstants.TEST_USER_2.uid())
+                .param("otherUid", TestConstants.TEST_USER_1.uid()));
+
+        mockMvc.perform(patch("/user/social/connect")
+                .param("fromUid", TestConstants.TEST_USER_1.uid())
+                .param("otherUid", TestConstants.TEST_USER_2.uid()))
+            .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void blocking_user_returns_accepted() throws Exception {
+        mockMvc.perform(patch("/user/social/block")
+                .param("fromUid", TestConstants.TEST_USER_1.uid())
+                .param("otherUid", TestConstants.TEST_USER_2.uid()))
+            .andExpect(status().isAccepted());
+    }
+
+    @Test
+    void unblocking_user_that_was_not_blocked_returns_not_found() throws Exception {
+        mockMvc.perform(patch("/user/social/unblock")
+                .param("fromUid", TestConstants.TEST_USER_1.uid())
+                .param("otherUid", TestConstants.TEST_USER_2.uid()))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void search_users_returns_ok() throws Exception {
+        mockMvc.perform(get("/user/social/search")
+                .param("fromUid", TestConstants.TEST_USER_1.uid())
+                .param("searchQuery", "Test"))
+            .andExpect(status().isOk());
+    }
+}
