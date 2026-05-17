c4a7d6a9d438ced38e06cb5c5cbf54a312711b54
Jason3859 <augustinej548@gmail.com>
2026-05-17 13:21:36 +0530

    set priority to high

--- messaging-microservice/src/main/java/dev/jason/project/spring/vc_server/microservice/messaging/repo/messaging/MessagingRepoImpl.java
+++ messaging-microservice/src/main/java/dev/jason/project/spring/vc_server/microservice/messaging/repo/messaging/MessagingRepoImpl.java
@@ -5,10 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
+import com.google.firebase.messaging.AndroidConfig;
+import com.google.firebase.messaging.AndroidConfig.Priority;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.MessagingErrorCode;
 
 import dev.jason.project.spring.vc_server.core.model.Device;
 import dev.jason.project.spring.vc_server.core.model.User;
@@ -25,9 +26,16 @@ public class MessagingRepoImpl implements MessagingRepository {
             .setToken(device.getToken())
             .putData("type", forDeviceCheck ? "check" : "message")
             .putAllData(message.asMap())
+            .setAndroidConfig(
+            	AndroidConfig.builder()
+            		.setPriority(Priority.HIGH)
+            		.build()
+            )
             .build();
 
         sendMessage(firebaseMessage);
+        
+        logger.info("sent message {} to {}", message.text(), device);
     }
 
     @Override
@@ -46,11 +54,8 @@ public class MessagingRepoImpl implements MessagingRepository {
     	try {
     		FirebaseMessaging.getInstance().send(message);
     	} catch (FirebaseMessagingException e) {
-    		MessagingErrorCode errorCode = e.getMessagingErrorCode();
-    		
-    		if (!errorCode.equals(MessagingErrorCode.UNREGISTERED)) { // ignores test device instances in database
-				logger.error("Error occurred while sending message: {}", e.getMessage());
-			}
+    		logger.error("Error occurred while sending message: {}", e.getMessage());
+    		e.printStackTrace(System.err);
 		}
     }
 }
